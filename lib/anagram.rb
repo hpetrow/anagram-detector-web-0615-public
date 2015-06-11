fdc4f01eb456d4fb9de5a0d0aa2d2252c362cf56
hpetrow <heather.petrow@gmail.com>
2015-06-11 16:31:01 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+    @anagrams = word.split(//).permutation.to_a.collect { |permutation|
+      permutation.join
+    }
+  end
+
+  def match(words)
+    words.select{ |word|
+      @anagrams.include?(word)
+    }
+  end
+end
